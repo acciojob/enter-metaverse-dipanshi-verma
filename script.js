3b35ff02abf120b393f3a53a0f4c938dab42b158
Dipanshi Verma <dipanshiverma0@gmail.com>
2025-06-18 18:07:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-document.getElementById("enterBtn").addEventListener("click", function() {
+document.getElementById("enterBtn").addEventListener("click", function () {
   const statusPara = document.getElementById("status");
 
-  // Replace <p> content with <h1>Entered Metaverse</h1>
-  statusPara.outerHTML = "<h1 id='status'>Entered Metaverse</h1>";
+  const newHeading = document.createElement("h1");
+  newHeading.id = "status";
+  newHeading.textContent = "Entered Metaverse";
+
+  statusPara.replaceWith(newHeading);
 });
